af74f06745e0ace8e803888d205f41461e1abea1
Angus Findlay <angus.findlay@icloud.com>
2023-01-30 21:23:23 +0000

    reducer

--- src/FiveSevenFive.tsx
+++ src/FiveSevenFive.tsx
@@ -1,6 +1,36 @@
+import { useReducer } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+type Haiku = [string, string, string];
+
+type State = { screen: "splash" } | { screen: "input"; haiku: Haiku };
+
+type Action =
+  | { type: "start" }
+  | { type: "change_line"; line: 0 | 1 | 2; newLine: string }
+  | { type: "submit" };
+
+const defaultHaiku: Haiku = ["", "", ""];
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "change_line":
+      if (state.screen == "input") {
+        return {
+          ...state,
+          haiku: { ...state.haiku, [action.line]: action.newLine },
+        };
+      } else {
+        return state;
+      }
+  }
+};
+
 export const FiveSevenFive = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    screen: "splash",
+  });
+
   return (
     <View>
       <Text style={styles.number}>5</Text>
