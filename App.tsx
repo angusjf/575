cddf7d32e4d7267cf759186198b30522c175aa65
Angus Findlay <angus.findlay@icloud.com>
2023-01-30 20:54:02 +0000

    575

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>5</Text>
+      <Text>7</Text>
+      <Text>5</Text>
       <StatusBar style="auto" />
     </View>
   );
