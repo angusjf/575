fbb9a3f6706483bbf1ea6ad548b03a52013d2c02
Angus Findlay <angus.findlay@icloud.com>
2023-02-19 20:54:53 +0000

    settings page

---
+++ src/components/Settings.tsx
@@ -0,0 +1,61 @@
+import { useMemo } from "react";
+import {
+  FlatList,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+} from "react-native";
+
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: "#fff",
+  },
+});
+
+type SettingsItemProps = {
+  title: string;
+  onPress: () => void;
+};
+
+const SettingsItem = ({ title, onPress }: SettingsItemProps) => (
+  <TouchableOpacity onPress={onPress}>
+    <View
+      style={{
+        height: 42,
+        borderColor: "lightgrey",
+        borderBottomWidth: 1,
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ paddingHorizontal: 22 }}>{title}</Text>
+    </View>
+  </TouchableOpacity>
+);
+
+type SettingsProps = { logout: () => void };
+
+export const Settings = ({ logout }: SettingsProps) => {
+  const settings = useMemo(
+    () => [
+      { title: "Logout", onPress: logout },
+      { title: "Unblock users", onPress: () => undefined },
+      { title: "Delete your account", onPress: () => undefined },
+    ],
+    []
+  );
+
+  return (
+    <SafeAreaView style={styles.root}>
+      <View>
+        <FlatList
+          data={settings}
+          renderItem={({ item }) => <SettingsItem {...item} />}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
