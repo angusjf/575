06a5c23f6ea469fbe4be2f864584ecf08c729af3
Angus Findlay <angus.findlay@icloud.com>
2023-02-24 19:42:32 +0000

    duct tape root app navigation

--- App.tsx
+++ App.tsx
@@ -3,23 +3,25 @@ import { RegisterForm } from "./src/components/RegisterForm";
 import { AppStateProvider, useAppState } from "./src/useAppState";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { FeedStack } from "./src/components/FeedStack";
+import { Text } from "react-native";
 
 const UnwrappedApp = () => {
   const { state } = useAppState();
 
-  if (state.state === "loading" || state.state === "finding_out_if_posted") {
-    return null;
+  switch (state.state) {
+    case "loading":
+    case "finding_out_if_posted":
+      return null;
+    case "register":
+      return <RegisterForm />;
+    case "compose":
+      return <HaikuForm />;
+    case "feed":
+    case "settings":
+      return <FeedStack />;
+    case "error":
+      return <Text>{state.message}</Text>;
   }
-
-  if (state.state === "register") {
-    return <RegisterForm />;
-  }
-
-  if (state.state === "compose") {
-    return <HaikuForm />;
-  }
-
-  return <FeedStack />;
 };
 
 export default function App() {
