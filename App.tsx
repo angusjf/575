4d56aa0de202ef70fea1f6df17f7add30658107a
Angus Findlay <angus.findlay@icloud.com>
2023-03-11 19:59:24 +0000

    ignore warning

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { NavigationContainer } from "@react-navigation/native";
+import { LogBox } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { RootStack } from "./src/components/RootStack";
 import { AppStateProvider } from "./src/useAppState";
+
+LogBox.ignoreLogs(["AsyncStorage has been extracted from react-native core"]);
 
 export default function App() {
   return (
