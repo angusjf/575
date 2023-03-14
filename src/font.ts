c5a3cbe22b89812db554767038cb086f931d63c1
Angus Findlay <angus.findlay@icloud.com>
2023-03-14 21:56:04 +0000

    fonts

--- src/font.ts
+++ src/font.ts
@@ -1,16 +1,20 @@
 import { loadAsync, useFonts } from "expo-font";
 
 export const fonts = {
-  PlexSerifRegular: "PlexSerifRegular",
-  PlexSerifBoldItalic: "PlexSerifBoldItalic",
-  PlexMonoItalic: "IBMPlexMono-Italic",
   PlexMonoBold: "IBMPlexMono-Bold",
+  PlexMonoBoldItalic: "IBMPlexMono-BoldItalic",
+  PlexMonoItalic: "IBMPlexMono-Italic",
+  PlexSansBoldItalic: "IBMPlexSans-BoldItalic",
+  PlexSerifBoldItalic: "IBMPlexSerif-BoldItalic",
+  PlexSerifRegular: "IBMPlexSerif-Regular",
 };
 
 export const loadFonts = () =>
   loadAsync({
-    [fonts.PlexSerifRegular]: require("../assets/fonts/IBMPlexSerif-Regular.ttf"),
-    [fonts.PlexSerifBoldItalic]: require("../assets/fonts/IBMPlexSerif-BoldItalic.ttf"),
-    [fonts.PlexMonoItalic]: require("../assets/fonts/IBMPlexMono-Italic.ttf"),
     [fonts.PlexMonoBold]: require("../assets/fonts/IBMPlexMono-Bold.ttf"),
+    [fonts.PlexMonoBoldItalic]: require("../assets/fonts/IBMPlexMono-BoldItalic.ttf"),
+    [fonts.PlexMonoItalic]: require("../assets/fonts/IBMPlexMono-Italic.ttf"),
+    [fonts.PlexSansBoldItalic]: require("../assets/fonts/IBMPlexSans-BoldItalic.ttf"),
+    [fonts.PlexSerifBoldItalic]: require("../assets/fonts/IBMPlexSerif-BoldItalic.ttf"),
+    [fonts.PlexSerifRegular]: require("../assets/fonts/IBMPlexSerif-Regular.ttf"),
   });
