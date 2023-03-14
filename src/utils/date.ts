5da7671b241e33414d2beb8dc58f848425edfcf2
Daniel Belo Goncalves <danigo99@gmail.com>
2023-03-14 11:40:19 +0000

    Feed redesign

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -4,3 +4,20 @@ export const dateDbKey = (date: Date) =>
 export const parseDateDbKey = (date: string) => {
   return new Date(date.replace(/-/g, "/"));
 };
+
+export const timestampToRelative = (timestamp: number) => {
+  const timeNow = new Date();
+  const date = new Date(timestamp);
+  const timeDiff = timeNow.getTime() - date.getTime();
+  const timeDiffInHours = timeDiff / 1000 / 60 / 60;
+  const timeDiffInDays = timeDiffInHours / 24;
+  const timeDiffInMins = timeDiff / 1000 / 60;
+
+  if (timeDiffInHours < 1) {
+    return `${Math.round(timeDiffInMins)} mins ago`;
+  } else if (timeDiffInDays < 1) {
+    return `${Math.round(timeDiffInHours)} hours ago`;
+  } else {
+    return `${Math.round(timeDiffInDays)} days ago`;
+  }
+};
