ffe46ef43faeae005133b9275b0e6ac85eb5fd1e
Angus Findlay <angus.findlay@icloud.com>
2023-05-08 09:42:28 +0100

    Update src/utils/notableLocations.ts
    Co-authored-by: Daniel Belo Goncalves <47107427+danigo99@users.noreply.github.com>

--- src/utils/notableLocations.ts
+++ src/utils/notableLocations.ts
@@ -1,4 +1,4 @@
 export const notableLocations: Partial<Record<string, string>> = {
-  "155 Wardour St": "Theodo UK",
+  "151 Wardour St": "Theodo UK",
   "Pettie House, Townshend Estate": "575 Headquarters",
 };
